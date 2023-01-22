543837b32f181c1127c6886cc16dd54ce9cb3fdf
Abhi <kaushikabhishek87@gmail.com>
2023-01-22 21:48:40 +0530

    add requirements, add pyflink dependencies

--- TableAPI_SQL_1.py
+++ TableAPI_SQL_1.py
@@ -4,4 +4,4 @@ env_settings = EnvironmentSettings.in_batch_mode()
 table_env = TableEnvironment.create(env_settings)
 
 table = table_env.from_elements([(1, 'Hi'), (2, 'Hello')])
-table.execute().print()
+table.execute().print() 
